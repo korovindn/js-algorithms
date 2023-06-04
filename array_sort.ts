6e1c4a2e3ff1cdaf9d5377c02d875ae93129936d
Дмитрий Коровин <dmitrijkorovin@Dmitrijs-MacBook-Air.local>
2023-06-04 15:19:25 +0300

    + quick sort

--- array_sort.ts
+++ array_sort.ts
@@ -15,5 +15,25 @@ const bubbleSort = <T>(
   return res;
 };
 
-console.log(bubbleSort<number>(toSortArray)) // works good, but min O(n) max O(n^2)
+console.log(bubbleSort<number>(toSortArray)); // works good, but min O(n) max O(n^2)
 
+const quickSort = <T>(
+  array: T[],
+  sorter: (a: T, b: T) => 1 | -1 = (a, b) => (a < b ? -1 : 1)
+): T[] => {
+  if (array.length <= 1) {
+    return array;
+  }
+  const pivotIndex = Math.floor(array.length / 2);
+  const pivot = array[pivotIndex];
+  const less = [];
+  const greater = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i === pivotIndex) continue;
+    if (sorter(array[i], pivot) === 1) greater.push(array[i])
+    else less.push(array[i])
+  }
+  return [...quickSort(less), pivot, ...quickSort(greater)]
+};
+
+console.log(quickSort<number>(toSortArray))
